409eb6cd324c45dc7431840744579d1f0660a8ef
RacFor4k <racfor4@gmail.com>
2024-04-15 10:15:36 +0300

    123

--- BlazorApp3/BlazorApp3/Controllers/ValuesController.cs
+++ BlazorApp3/BlazorApp3/Controllers/ValuesController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorApp3.Modules;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace BlazorApp3.Controllers
@@ -9,9 +12,23 @@ namespace BlazorApp3.Controllers
     [ApiController]
     public class GmailController : ControllerBase 
     {
-        [HttpGet("{mail}/{key}")]
-        public async Task<IActionResult> SendMail(string mail, string key)
+        private byte[] key = SHA256.HashData(Encoding.UTF8.GetBytes("123"));
+        string host = "localhost:";
+
+        [HttpGet("Link/{login}")]
+        public async Task<IActionResult> Link(string login) {
+            login = EncryptionHelper.DecryptString(login,key);
+
+                }
+
+        [HttpGet("{mail}/{login}")]
+        public async Task<IActionResult> SendMail(string mail, string login)
         {
+            System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create(); 
+            aes.Mode = CipherMode.CBC;
+            aes.Key = key;
+            aes.GenerateIV();
+            string link = host + "/api/Link/" + EncryptionHelper.EncryptString(login, key);
             string body;
             using (FileStream fs = new FileStream("GmailAPI/GmailAttachment/gmailReqest.html", FileMode.Open))
             {
@@ -19,9 +36,10 @@ namespace BlazorApp3.Controllers
                 await fs.ReadAsync(buffer, 0, (int)fs.Length);
                 body = Encoding.UTF8.GetString(buffer);
             }
-            var byteArray = "verification key VNCloud";
+
+            var byteArray = "verification link VNCloud";
             body = body.Replace("EmailInsert", mail);
-            body = body.Replace("KeyInsert", key);
+            body = body.Replace("KeyInsert", link);
 
 
             GmailMessage message = new GmailMessage(byteArray,new List<string> { mail },body);
---
+++ BlazorApp3/BlazorApp3/Modules/Encoder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorApp3.Modules
+{
+    static public class EncryptionHelper
+    {
+        public static string EncryptString(string text, byte[] key)
+        {
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Key = key;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter sw = new StreamWriter(cs))
+                            {
+                                sw.Write(text);
+                            }
+                        }
+
+                        byte[] iv = aes.IV;
+                        byte[] encrypted = ms.ToArray();
+                        byte[] result = new byte[iv.Length + encrypted.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(encrypted, 0, result, iv.Length, encrypted.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText, byte[] key)
+        {
+            byte[] fullCipher = Convert.FromBase64String(cipherText);
+            byte[] iv = new byte[16];
+            byte[] cipher = new byte[fullCipher.Length - iv.Length];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    using (MemoryStream ms = new MemoryStream(cipher))
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader reader = new StreamReader(cs))
+                            {
+                                return reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
