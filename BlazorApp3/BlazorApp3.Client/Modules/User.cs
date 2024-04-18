2e0dd3225670f3e04ffe3cfae729dc15b0415673
k0lan4ik <kolasynnikolai@gmail.com>
2024-04-18 16:23:10 +0300

    Файловая система

--- BlazorApp3/BlazorApp3.Client/Modules/User.cs
+++ BlazorApp3/BlazorApp3.Client/Modules/User.cs
@@ -2,6 +2,7 @@
 {
     public static class User
     {
+        public static HttpClient HttpClient { get; } = new HttpClient();
         public static int Id { get; set; }
         public static string Jwt { get; set; }
         public static byte[] Login { get; set; }
