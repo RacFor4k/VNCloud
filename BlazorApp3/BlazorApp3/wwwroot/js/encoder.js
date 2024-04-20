8dd25e4b01a686c1d1b6160778960bfae1af6147
RacFor4k <racfor4@gmail.com>
2024-04-20 15:41:42 +0300

    js сюк

--- BlazorApp3/BlazorApp3/wwwroot/js/encoder.js
+++ BlazorApp3/BlazorApp3/wwwroot/js/encoder.js
@@ -1,20 +1,21 @@
 ﻿window.encryptAES = (byteArray, key) => {
     // Используйте библиотеку CryptoJS для шифрования
     var wordArray = CryptoJS.lib.WordArray.create(byteArray);
-    var encrypted = CryptoJS.AES.encrypt(wordArray, key).toString();
+    var iv = CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f'); // Статический IV
+    var encrypted = CryptoJS.AES.encrypt(wordArray, key, { iv: iv }).toString();
     return encrypted;
 }
 
 window.decryptAES = (encryptedData, key) => {
     // Используйте библиотеку CryptoJS для расшифровывания
-    var decrypted = CryptoJS.AES.decrypt(encryptedData, key);
-    var typedArray = new Uint8Array(decrypted.words.length * 4);
-    var offset = 0;
-    decrypted.words.forEach(function (word) {
-        typedArray[offset++] = (word >> 24) & 0xff;
-        typedArray[offset++] = (word >> 16) & 0xff;
-        typedArray[offset++] = (word >> 8) & 0xff;
-        typedArray[offset++] = word & 0xff;
+    var iv = CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f'); // Тот же статический IV
+    var decrypted = CryptoJS.AES.decrypt(encryptedData, key, { iv: iv });
+    var typedArray = new Uint8Array(decrypted.sigBytes);
+    decrypted.words.forEach((word, index) => {
+        typedArray[index * 4] = (word >> 24) & 0xff;
+        typedArray[index * 4 + 1] = (word >> 16) & 0xff;
+        typedArray[index * 4 + 2] = (word >> 8) & 0xff;
+        typedArray[index * 4 + 3] = word & 0xff;
     });
     return typedArray;
 }
